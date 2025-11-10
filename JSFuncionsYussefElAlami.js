6c4d7e2ac6e28b7395e0e3813a3c0bdd9ea48d6f
yeadr <yussef.elalami@institutvidreres.cat>
2025-11-10 11:09:41 +0100

    Ex 2

---
+++ JSFuncionsYussefElAlami.js
@@ -0,0 +1,14 @@
+function ex1(nom, cognoms) {
+    console.log("Ex 1:")
+    console.log(`${nom} ${cognoms}`);
+}
+
+ex1("Yussef", "El Alami")
+
+function ex2(variable) {
+    console.log("")
+    console.log("Ex 2:")
+    console.log(typeof variable);
+}
+
+ex2(33)
